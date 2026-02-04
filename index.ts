d6e24002309a8103fe7e6acdfe870c5e5446b667
Clarence Lewis <cel3media@gmail.com>
2026-02-03 21:20:03 -0500

    updaed index with FitRequest

--- index.ts
+++ index.ts
@@ -2,10 +2,12 @@ import { project } from "./project";
 import { service } from "./service";
 import { testimonial } from "./testimonial";
 import { siteSettings } from "./siteSettings";
+import { fitRequest } from "./schemaTypes/fitRequest";
 
 export const schemaTypes = [
   project,
   service,
   testimonial,
+  fitRequest,
   siteSettings,
 ];
